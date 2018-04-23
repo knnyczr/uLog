8dee56307503929671d4ed19bfe136dd10577f5f
KennyCruzer <kennycruz@ool-ad020785.dyn.optonline.net>
2018-04-23 16:05:39 -0400

    heroku update

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,5 +1,9 @@
-module.exports = {
-  host: 'localhost',
-  port: 5432,
-  database: 'ulog',
+const pg = require('pg-promise')();
+const config = {
+ host: 'localhost',
+ port: 5432,
+ database: 'ulog'
 }
+const db = pg(process.env.DATABASE_URL || config);
+
+module.exports = db;
